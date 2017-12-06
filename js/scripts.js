74228ad340733fae069524e72e817864c02a50aa
Jason Domican <jadomican@gmail.com>
2017-12-06 14:21:53 +0000

    Add Google search bar, smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    $('body').scrollspy({ target: ".navbar", offset: 50 });
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#myNavbar a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 380, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         // mobile code..
     }
-    else
-    {
+    else {
         // Don't use tooltips on mobile (would require double tap)
-        $('[data-toggle="tooltip"]').tooltip();         
+        $('[data-toggle="tooltip"]').tooltip();
     }
 
-    $('.contact-submit').click(function(){
+    $('.contact-submit').click(function () {
         $('.alert').show()
         console.log("clicked");
-    }) 
-
+    })
 });
